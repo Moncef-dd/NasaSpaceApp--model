ccf75b78107307bb4f6b7b92db072d7016557b82
Moncef SAMAI <moncef.samai.rol2@gmail.com>
2022-10-01 02:48:35 +0100

    assets input init

---
+++ assets.py
@@ -0,0 +1,7 @@
+from stylingcont import show_images_with_objects, load_images
+
+content_path = '/content/data/content/merc4_m10.jpg'
+style_path = '/content/data/style/download.jpeg'
+
+content_image, style_image = load_images(content_path, style_path)
+show_images_with_objects([content_image, style_image], titles=['content image', 'style image'])
